8bcddcf7c9cb7896f05d88616e2c4061936e342a
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-04 20:11:34 -0700

    user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,51 @@
-const router = require('express').Router();
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: ['/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/', 'Please fill a valid email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    }
+)
+
+userSchema.virtual('friends')
+.get(() => {
+    return (this.username).length;
+})
+.set((value) => {
+    this.set({value})
+})
 
 
-module.exports = router;
+const User = model('user', userSchema);
+
+module.exports = User;
