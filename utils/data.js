f77f9e89a0021bbf1e4ec84b74687529682952fa
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-04 22:23:31 -0700

    created data and started seeds file

---
+++ utils/data.js
@@ -0,0 +1,14 @@
+data = [
+    {
+        "username": "ramita",
+        "email": "ramita@example.com"
+    },
+    {
+        "username": "sagar",
+        "email": "sagar@example.com"
+    },
+    {
+        "username": "rajul",
+        "email": "rajul@example.com"
+    }
+]
---
+++ utils/seed.js
@@ -0,0 +1,22 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+
+connection.once('open', async () => {
+    console.log('connected');
+      // Delete the collections if they exist
+      let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+      if (userCheck.length) {
+        await connection.dropCollection('users');
+      }
+  
+      let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+      if (thoughtCheck.length) {
+        await connection.dropCollection('thoughts');
+      }
+  
+    // Log out the seed data to indicate what should appear in the database
+    console.table(students);
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
+  });
+  
