9393d84fcdec09fad85fef8964719721cd1b2141
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-04 21:17:36 -0700

    user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require ('../../controllers/userController');
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser)
+    .delete(deleteUser);
+
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser);
 
 module.exports = router;
